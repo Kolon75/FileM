f932f07564d3582b83c73cdaf852ee5d0ee94a9b
Kolon75 <gumbatoff.emil@yandex.ru>
2023-05-14 16:28:25 +0300

    .

--- Test/Properties/Resources.Designer.cs
+++ Test/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace t.Properties {
+namespace FileManager.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace t.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("t.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileManager.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
